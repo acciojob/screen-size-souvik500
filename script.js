bd64fea1a45d2fcfe5ddd723eddbbf609782d804
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-06-17 13:03:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function getScreenSize() {
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	document.getElementById("sizeInfo").innerText = `Width: ${width} and Height: ${height}`
+}
+
+window.addEventListener("resize", getScreenSize);
